ae9e306a51f367d865fea127ba8cc928bdb20f69
Alex <dinghua@me.com>
2015-03-23 17:55:34 +0800

    minor

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,20 +1,28 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        // initial admin user
+        $user        = new User();
+        $user->name  = 'admin';
+        $user->email = 'admin@me.com';
+        $user->password  = 'admin';
+
+
+        // initial admin role and permission
+    }
 
 }
